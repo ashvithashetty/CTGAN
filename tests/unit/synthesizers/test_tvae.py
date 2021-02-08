21eba0106d0d484a49e15492a010b40f9ae79dcd
Felipe Hofmann <fealho@gmail.com>
2021-02-08 01:45:57 -0800

    Updates tests documentation

--- tests/unit/synthesizers/test_tvae.py
+++ tests/unit/synthesizers/test_tvae.py
@@ -3,86 +3,122 @@ from ctgan.synthesizers import TVAESynthesizer, Decoder, Encoder, loss_function
 
 class TestEncoder(TestCase):
 
-    # METHOD: __init__(self, data_dim, compress_dims, embedding_dim)
-    # VALIDATE:
-    #     * assigned values
     def test___init__(self):
-        pass
+        """Test `__init__` for a generic case.
 
-    # METHOD: forward(self, input)
-    # VALIDATE:
-    #     * returned values
-    def test_forward(self):
-        """Make sure it computes the gradients for all modules, and check
-           whether the std is positive."""
-        pass
+        Make sure 'self.seq' has same length as 2*`compress_dims`.
+
+        Setup:
+            - Create Encoder
+
+        Input:
+            - data_dim = positive integer
+            - compress_dims = list of integers
+            - embedding_dim = positive integer
+
+        Output:
+            - None
+
+        Side Effects:
+            - Set `self.seq`, `self.fc1` and `self.fc2`
+        """
+
+    def test_forward(self): 
+        """Test `test_forward` for a generic case.
+
+        Check that the output shapes are correct and that std is positive.
+        We can also test that all parameters have a gradient attached to them
+        by running `encoder.parameters()`. To do that, we just need to use `loss.backward()`
+        for some loss, like `loss = torch.mean(mu) + torch.mean(std) + torch.mean(logvar)`.
+
+        Setup:
+            - Create random tensor
+
+        Input: 
+            - input = random tensor of shape (N, data_dim)
+
+        Output: 
+            - Tuple of (mu, std, logvar):
+              mu - tensor of shape (N, embedding_dim)
+              std - tensor of shape (N, embedding_dim), non-negative values
+              logvar - tensor of shape (N, embedding_dim)
+        """
 
 
 class TestDecoder(TestCase):
 
-    # METHOD: __init__(self, embedding_dim, decompress_dims, data_dim)
-    # VALIDATE:
-    #     * assigned values
     def test___init__(self):
-        pass
+        """Test `__init__` for a generic case.
 
-    # METHOD: forward(self, input)
-    # VALIDATE:
-    #     * returned values
-    def test_forward(self):
-        """Make sure it computes the gradients for all modules, and check
-           whether the std is positive."""
-        pass
+        Make sure 'self.seq' has same length as 2*`decompress_dims` + 1.
+
+        Setup:
+            - Create Decoder
+
+        Input:
+            - data_dim = positive integer
+            - decompress_dims = list of integers
+            - embedding_dim = positive integer
+
+        Output:
+            - None
+
+        Side Effects:
+            - Set `self.seq`, `self.sigma`
+        """
 
 
 class TestLossFunction(TestCase):
 
-    # METHOD: loss_function(recon_x, x, sigmas, mu, logvar, output_info, factor)
-    # VALIDATE:
-    #     * assigned values
     def test_loss_function(self):
-        """ """
+        """Test 'loss_function'
+
+        Check loss values = to specific numbers.
+
+        Setup:
+            Build all the tensors, lists, etc.
+
+        Input:
+            recon_x = tensor of shape (N, data_dims)
+            x = tensor of shape (N, data_dims)
+            sigmas = tensor of shape (N,)
+            mu = tensor of shape (N,)
+            logvar = tensor of shape (N,)
+            output_info = list of SpanInfo objects from the data transformer, including at least 1 continuous and 1 discrete
+            factor = scalar
+
+        Output:
+            reconstruction loss = scalar = f(recon_x, x, sigmas, output_info, factor)
+            kld loss = scalar = f(logvar, mu)
+        """
         pass
 
 
 class TestTVAESynthesizer(TestCase):
 
-    # METHOD: __init__(
-    #    self,
-    #    embedding_dim=128,
-    #    compress_dims=(128, 128),
-    #    decompress_dims=(128, 128),
-    #    l2scale=1e-5,
-    #    batch_size=500,
-    #    epochs=300)
-    # VALIDATE:
-    #     * assigned values
-    def test___init__(self):
-        """ """
-        pass
-
-
-    # METHOD: fit(self, train_data, discrete_columns=tuple())
-    # VALIDATE:
-    #     * returned values
-    def test_fit(self):
-        """ """
-        pass
-
-
-    # METHOD: _sample(self, samples)
-    # VALIDATE:
-    #     * returned values
-    def test_sample(self):
-        """ """
-        pass
-
-
-    # METHOD: set_device(self, device)
-    # VALIDATE:
-    #     * returned values
     def test_set_device(self):
-        """ """
+        """Test 'set_device' if a GPU is available.
+
+        Check that decoder/encoder can successfully be moved to the device.
+        If the machine doesn't have a GPU, this test shouldn't run.
+
+        Setup:
+            - Move decoder/encoder to device
+
+        Input:
+            - device = string
+
+        Output:
+            None
+
+        Side Effects:
+            - Set `self._device` to `device`
+            - Moves `self.decoder` to `self._device`
+
+        Note:
+            - Need to be careful when checking whether the encoder is actually set
+            to the right device, since it's not saved (it's only used in fit).
+        """
         pass
 
 
